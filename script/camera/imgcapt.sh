c328a0dfb34a180862291d5f0be70061632f20d5
Joe Giannitti <giannitj2@hawkmail.newpaltz.edu>
2017-09-13 23:06:12 -0400

    Updated directory references

--- script/camera/imgcapt.sh
+++ script/camera/imgcapt.sh
@@ -2,6 +2,6 @@
 
 DATE=$(date +"%Y-%m-%d_%H%M")
 
-fswebcam -r 1280x1024 --no-banner --log /home/pi/libproject/script/camera/imgcapt.log /home/pi/libproject/files/images/$DATE.jpg
+fswebcam -r 1280x1024 --no-banner --log /home/pi/spine-reader/script/camera/imgcapt.log /home/pi/spine-reader/files/images/$DATE.jpg
 
 
