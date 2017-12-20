59fc5195a76090000fe14d302bfc7faa07a2573d
Joe Giannitti <giannitj2@hawkmail.newpaltz.edu>
2017-12-20 12:14:29 -0500

    Update imgcapt.sh

--- script/camera/imgcapt.sh
+++ script/camera/imgcapt.sh
@@ -2,6 +2,6 @@
 
 DATE=$(date +"%Y-%m-%d_%H%M")
 
-sudo fswebcam -r 1280x1024 --no-banner --log /home/pi/spine-reader/script/camera/imgcapt.log /home/pi/spine-reader/files/images/$DATE.jpg
+sudo fswebcam -r 1280x1024 --no-banner --log /home/pi/clara/script/camera/imgcapt.log /home/pi/clara/files/images/$DATE.jpg
 
 
